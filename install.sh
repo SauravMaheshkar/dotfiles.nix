79f0358293dfe91739c47e449d488416168039f7
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2022-12-29 19:56:33 +0000

    fix(install): use the docker install script instead

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 os_type=$(uname -s)
 
 apt_packages="build-essential curl gcc gh git gfortran libopenblas-dev neofetch python3.8 python3.8-dev python3.8-pip python3.8-venv stow tmux unzip zsh"
-apt_packages_optional="docker-ce docker-ce-cli containerd.io docker-compose-plugin"
+apt_packages_optional=""
 
 brew_packages="cmake gcc git gnutls llvm mysql-client neofetch pybind11 python@3.8 stow tmux tree wget vim"
 brew_packages_optional="docker gh git-lfs hdf5 icarus-verilog numpy openblas qemu readline ruby ruby-build scipy six sqlite"
@@ -14,7 +14,11 @@ brew_packages_optional="docker gh git-lfs hdf5 icarus-verilog numpy openblas qem
 ###############################################################################
 
 function apt_install_packages {
-    sudo apt-get update && sudo apt-get install -y ${apt_packages} ${apt_packages_optional}
+    sudo apt-get update && sudo apt-get install -y --no-install-recommends ${apt_packages} ${apt_packages_optional} && apt-get clean && rm -rf /var/lib/apt/lists/* && apt_install_docker
+}
+
+function apt_install_docker {
+    /bin/bash -c "$(curl -fsSL https://get.docker.com -o get-docker.sh)" && sh get-docker.sh
 }
 
 function brew_install_self {

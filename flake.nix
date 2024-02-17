a0dc86ef50a1c804ee24020558cb1f04cc94bd63
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2024-02-17 14:17:20 +0000

    style: lint using nixfmt

--- flake.nix
+++ flake.nix
@@ -1,22 +1,19 @@
 {
-	description = "Personal Development Environment.";
+  description = "Personal Development Environment.";
 
-	inputs = {
-		darwin.inputs.nixpkgs.follows = "nixpkgs";
-		darwin.url = "github:lnl7/nix-darwin";
-		home-manager.inputs.nixpkgs.follows = "nixpkgs";
-		home-manager.url = "github:nix-community/home-manager/release-23.11";
-		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-	};
+  inputs = {
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    darwin.url = "github:lnl7/nix-darwin";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
 
-	outputs = inputs@{ self, darwin, home-manager, nixpkgs, ... }:
-		let
-			username = "saurav";
-			darwin-system = import ./system/darwin.nix { inherit inputs username; };
-		in
-		{
-			darwinConfigurations = {
-				aarch64 = darwin-system "aarch64-darwin";
-			};
-		};
+  outputs = inputs@{ self, darwin, home-manager, nixpkgs, ... }:
+    let
+      username = "saurav";
+      darwin-system = import ./system/darwin.nix { inherit inputs username; };
+    in {
+      darwinConfigurations = { aarch64 = darwin-system "aarch64-darwin"; };
+    };
 }
--- module/home-manager.nix
+++ module/home-manager.nix
@@ -1,129 +1,115 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
-let
-	userVimPlugin = pkgs.vimUtils.buildVimPlugin {
-		name = "config";
-		src = ../config/nvim;
-	};
-in
-	{
-		nixpkgs.config.allowUnfree = true;
-		home.enableNixpkgsReleaseCheck = false;
-		home.stateVersion = "23.11";
+{
+  nixpkgs.config.allowUnfree = true;
+  home.enableNixpkgsReleaseCheck = false;
+  home.stateVersion = "23.11";
 
-		home.packages = with pkgs; [
-			fd
-			ripgrep
-			coreutils
-			findutils
-			unzip
-			wget
-			git
-			nodejs
-			nodePackages.npm
-			neofetch
-			rectangle
-			python310
-			python310Packages.pip
-			cargo
-			clippy
-			rustc
-			rustfmt
-		];
+  home.packages = with pkgs; [
+    fd
+    ripgrep
+    coreutils
+    findutils
+    unzip
+    wget
+    git
+    nodejs
+    nodePackages.npm
+    neofetch
+    rectangle
+    python310
+    python310Packages.pip
+    cargo
+    clippy
+    rustc
+    rustfmt
+  ];
 
-		programs.direnv = {
-        		enable = true;
-        		enableZshIntegration = true;
-        		nix-direnv.enable = true;
-    		};
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+  };
 
-		programs.fzf = {
-        		enable = true;
-        		enableZshIntegration = true;
-        		defaultOptions = [
-            			"--preview 'cat {}'"
-        		];
-    		};
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+    defaultOptions = [ "--preview 'cat {}'" ];
+  };
 
-		programs.git = {
-			enable = true;
-			userName = "Saurav Maheshkar";
-			userEmail = "sauravvmaheshkar@gmail.com";
+  programs.git = {
+    enable = true;
+    userName = "Saurav Maheshkar";
+    userEmail = "sauravvmaheshkar@gmail.com";
 
-			lfs = { enable = true; };
+    lfs = { enable = true; };
 
-			ignores = [];
+    ignores = [ ];
 
-			aliases = {
-				"s" = "status";
-				"del" = "branch -D";
-			};
+    aliases = {
+      "s" = "status";
+      "del" = "branch -D";
+    };
 
-			extraConfig = {
-				commit.gpgsign = true;
-				protocol.keybase.allow = "always";
-				credential.helper = "osxkeychain";
-				init.defaultBranch = "main";
-				user = { signingkey = "5A487B40E23BAA71"; };
-			};
-		};
+    extraConfig = {
+      commit.gpgsign = true;
+      protocol.keybase.allow = "always";
+      credential.helper = "osxkeychain";
+      init.defaultBranch = "main";
+      user = { signingkey = "5A487B40E23BAA71"; };
+    };
+  };
 
-		programs.gh = {
-			enable = true;
-		};
+  programs.gh = { enable = true; };
 
-		programs.gpg = {
-			enable = true;
-		};
+  programs.gpg = { enable = true; };
 
-		programs.neovim = {
-			enable = true;
-			plugins = [
-				userVimPlugin
-			];
-			extraLuaConfig = ''
-				require('config.lazy')
-			'';
-			defaultEditor = true;
-			withPython3 = true;
-		};
+  xdg.configFile = {
+    nvim = {
+      source = config.lib.file.mkOutOfStoreSymlink ../config/nvim;
+      recursive = true;
+    };
+  };
 
-		programs.starship = {
-			enable = true;
-			enableZshIntegration = true;
-			settings = {
-				add_newline = false;
-				git_branch = { symbol = "🌱 "; };
-				nix_shell = {
-      					disabled = true;
-      					impure_msg = "[impure shell](bold red)";
-      					pure_msg = "[pure shell](bold green)";
-      					format = "via [☃️ $state( ($name))](bold blue) ";
-    				};
-				package = {
-					disabled = true;
-				};
-			};
-		};
+  programs.neovim = {
+    enable = true;
+    extraLuaConfig = "	require('config.lazy')\n";
+    defaultEditor = true;
+    withNodeJs = true;
+    withPython3 = true;
+    vimdiffAlias = true;
+  };
 
-		programs.tmux = {
-			enable = true;
-		};
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      add_newline = false;
+      git_branch = { symbol = "🌱 "; };
+      nix_shell = {
+        disabled = true;
+        impure_msg = "[impure shell](bold red)";
+        pure_msg = "[pure shell](bold green)";
+        format = "via [☃️ $state( ($name))](bold blue) ";
+      };
+      package = { disabled = true; };
+    };
+  };
 
-		programs.zsh = {
-			enable = true;
-			enableAutosuggestions = true;
-			autocd = false;
-			shellAliases = {
-				"c" = "clear";
-				"code" = "nvim";
-				"dwc" = ''darwin-rebuild check --flake ".#aarch64"'';
-				"dwb" = ''darwin-rebuild switch --flake ".#aarch64"'';
-				"xgc" = "nix-store --gc && nix-store --optimise";
-				"ls" = "ls -Alh --color=auto";
-			};
-			syntaxHighlighting = {
-				enable = true;
-			}; 
-		};		
+  programs.tmux = { enable = true; };
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    autocd = false;
+    shellAliases = {
+      "c" = "clear";
+      "code" = "nvim";
+      "dwc" = ''darwin-rebuild check --flake ".#aarch64"'';
+      "dwb" = ''darwin-rebuild switch --flake ".#aarch64"'';
+      "xgc" = "nix-store --gc && nix-store --optimise";
+      "ls" = "ls -Alh --color=auto";
+    };
+    syntaxHighlighting = { enable = true; };
+  };
 }

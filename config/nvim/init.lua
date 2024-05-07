17c84f8014c83d637e8b400415a1d98889e9e18e
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2024-05-07 16:03:40 +0100

    fix(nvim): set filetype correctly

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,16 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-vim.cmd([[
-  augroup filetypes
-    autocmd!
-    autocmd BufNewFile,BufRead *.pl set filetype=prolog
-  augroup END
-]])
-
-vim.cmd([[
-  augroup filetypes
-    autocmd!
-    autocmd BufNewFile,BufRead justfile set filetype=make
-  augroup END
-]])
-
 require("config")
--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.pl",
+  command = "set filetype=prolog",
+})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "justfile",
+  command = "set filetype=make",
+})

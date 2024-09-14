3c122db428a47e06ab524ec93d74bde63b7362f2
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2024-09-14 22:04:40 +0100

    chore: manage npm externally

--- shared/packages.nix
+++ shared/packages.nix
@@ -5,12 +5,10 @@ let
 
   buildTools = with pkgs; [ cmake just pkg-config pre-commit rustc ];
 
-  jsTools = (with pkgs; [ nodejs ] ++ (with nodePackages; [ pnpm ]));
-
   misc = with pkgs; [ neofetch stow gh glow tokei ];
 
   nixTools = with pkgs; [ nixfmt ];
 
   rustTools = with pkgs; [ cargo clippy ];
 in
-basic ++ buildTools ++ jsTools ++ misc ++ nixTools ++ rustTools
+basic ++ buildTools ++ misc ++ nixTools ++ rustTools
